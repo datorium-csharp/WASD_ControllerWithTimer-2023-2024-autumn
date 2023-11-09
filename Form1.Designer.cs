ececa0255298a0722cdf844bd041d8d467a4d6c8
& C:/Users/Elchin/AppData/Local/Programs/Python/Python310/python.exe c:/Users/Elchin/OneDrive/Documents/Codes/Python/json-thu1200/script.py <hello@datorium.eu>
2023-11-09 18:02:39 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace WASD_ControllerWithTimer_2023_2024_autumn
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.hero = new System.Windows.Forms.PictureBox();
+            this.timerMain = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.hero)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hero
+            // 
+            this.hero.BackColor = System.Drawing.Color.Fuchsia;
+            this.hero.Location = new System.Drawing.Point(439, 250);
+            this.hero.Name = "hero";
+            this.hero.Size = new System.Drawing.Size(100, 100);
+            this.hero.TabIndex = 0;
+            this.hero.TabStop = false;
+            // 
+            // timerMain
+            // 
+            this.timerMain.Interval = 50;
+            this.timerMain.Tick += new System.EventHandler(this.timerMain_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(1055, 624);
+            this.Controls.Add(this.hero);
+            this.Name = "Form1";
+            this.Text = "WASD Controller";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.hero)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox hero;
+        private System.Windows.Forms.Timer timerMain;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+namespace WASD_ControllerWithTimer_2023_2024_autumn
+{
+    public partial class Form1 : Form
+    {
+
+        private int speed = 5;
+        private int verticalComponent = 0; // 0: no move, 1: down, -1: up
+        private int horizontalComponent = 0; // 0: no move, 1: right, -1: left
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTimer();
+        }
+
+        private void InitializeTimer()
+        {
+            timerMain.Interval = 10;
+            timerMain.Start();
+        }
+
+        private void timerMain_Tick(object sender, EventArgs e)
+        {
+            HeroMove();
+            HeroWallCollider();
+        }
+
+        private void HeroMove()
+        {
+            hero.Left += horizontalComponent * speed;
+            hero.Top += verticalComponent * speed;
+        }
+
+
+        private void HeroWallCollider()
+        {
+            if(hero.Left > ClientRectangle.Width)
+            {
+                hero.Left = -hero.Width;
+            }
+            else if(hero.Left < -hero.Width)
+            {
+                hero.Left = ClientRectangle.Width;
+            }
+            
+            if(hero.Top > ClientRectangle.Height)
+            {
+                hero.Top = -hero.Height;
+            }
+            else if(hero.Top < -hero.Height)
+            {
+                hero.Top = ClientRectangle.Height;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.A) //moving to the left
+            {
+                horizontalComponent = -1;
+                verticalComponent = 0;
+            }
+            else if(e.KeyCode == Keys.D) //moving to the right
+            {
+                horizontalComponent = 1;
+                verticalComponent = 0;
+            }
+            else if(e.KeyCode == Keys.W) //moving to the top
+            {
+                verticalComponent = -1;
+                horizontalComponent = 0;
+            }
+            else if(e.KeyCode == Keys.S) //moving to the bottom
+            {
+                verticalComponent = 1;
+                horizontalComponent = 0;
+            }
+            else if(e.KeyCode == Keys.P) //pausing the game
+            {                
+                horizontalComponent = 0;
+                verticalComponent = 0;
+            }
+            else if (e.KeyCode == Keys.Add || e.KeyCode == Keys.Oemplus)
+            {
+                if (timerMain.Interval > 1)
+                {
+                    timerMain.Interval -= 1;
+                }                
+            }
+            else if (e.KeyCode == Keys.Subtract || e.KeyCode == Keys.OemMinus)
+            {
+                timerMain.Interval += 1;
+            }
+        }
+    }
+}
